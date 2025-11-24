9a3fcf101cf096660c8c39ca9841d1e38d5e425a
DLourenco18 <contact.lourenco18@gmail.com>
2025-11-24 11:34:54 +0000

    authentication with clerk

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 npm install @supabase/ssr @dnd-kit/core @dnd-kit/sortable @dnd-kit/utilities @radix-ui/react-dialog @radix-ui/react-label @radix-ui/react-select @radix-ui/react-slot lucide-react class-variance-authority tailwind-merge clsx @clerk/nextjs
 tw-animate-css
 npx shadcn@latest init
+
+supabase pass:RhVwEwC7w96SwP97e
---
+++ components/navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { ArrowRight, Trello } from "lucide-react";
+import { Button } from "./ui/button";
+import { Arrow } from "@radix-ui/react-select";
+import Link from "next/link";
+
+
+export default function Navbar() {
+    const { isSignedIn, user } = useUser();
+    return <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+        <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+            <div className="flex item-center space-x-2">
+                <Trello className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
+                <span className="text-xl sm:text-exl font-bold text-gray-900">Trello Clone</span>
+            </div>
+            <div className="flex items-center space-x-2 sm:space-x-4">
+                {isSignedIn ? (
+                    <div className="flex flex-col sm:flex-row items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4">
+                        <span className="text-xs sm:text-sm text-gray-600 hidden sm:block">
+                            Hello, {user?.firstName || "User"}!
+                        </span>
+                        <Link href="/dashboard">
+                            <Button variant="outline" size="sm" className="text-xs sm:text-sm">
+                                Go to Dashboard <ArrowRight />
+                            </Button>
+                        </Link>
+                    </div>)
+                    : (<div>
+                        <SignInButton>
+                            <Button variant="ghost" size="sm" className="text-gray-900 text-xs sm:text-sm">Sign In</Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button variant="outline" size="sm" className="text-xs sm:text-sm">Sign Up</Button>
+                        </SignUpButton>
+                    </div>)}
+
+            </div>
+        </div>
+    </header>;
+}
